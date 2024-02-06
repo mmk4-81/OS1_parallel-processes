b5475a3a160ae2c273f1d62d61117fe6a86ab958
mmk <mmkkarbalaei@gmail.com>
2024-02-06 23:20:16 +0330

    import dataset in project

--- Parent.cpp
+++ Parent.cpp
@@ -5,8 +5,20 @@
 #include <chrono>
 
 using namespace std;
+#define Filepath "C:\\Users\\Desktop\\dataset\\"
+
+
 
 int main() {
-    
+    string filename;
+    cout << "Enter dataset filename(1-6) : ";
+    cin >> filename;
+
+    ifstream file(Filepath + filename + ".txt");
+    if (!file) {
+        cout << "Cannot open file: " << filename << endl;
+        return 1;
+    }
+
     return 0;
 }
