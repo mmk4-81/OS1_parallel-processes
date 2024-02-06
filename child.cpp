14c540e7703fac213000957177ac5f8db76624e9
mmk <mmkkarbalaei@gmail.com>
2024-02-06 23:41:27 +0330

    Mapping them to variables

--- child.cpp
+++ child.cpp
@@ -16,5 +16,16 @@ int main()
     HANDLE hResult = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, TEXT("SharedMemory-result"));
     HANDLE hProcessInfo = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, TEXT("SharedMemory-ProcessInfo"));
 
+    double* sharedProcessInfo = (double*)MapViewOfFile(hProcessInfo, FILE_MAP_ALL_ACCESS, 0, 0, 3 * sizeof(double));
+
+    double totalLands = sharedProcessInfo[0];
+    int DataSize = sharedProcessInfo[1];
+    double runtime = sharedProcessInfo[2];
+
+    double* lands = (double*)MapViewOfFile(hData, FILE_MAP_ALL_ACCESS, 0, 0, DataSize * sizeof(double));
+    double* result = (double*)MapViewOfFile(hResult, FILE_MAP_ALL_ACCESS, 0, 0, Core * (DataSize + 4) * sizeof(double));
+
+
+
     return 0;
 }
