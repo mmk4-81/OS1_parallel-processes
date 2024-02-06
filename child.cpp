947f6491d8e4b85368841cf750d8f9a78af8e6dc
mmk <mmkkarbalaei@gmail.com>
2024-02-06 23:40:20 +0330

    OpenFilesMapping

--- child.cpp
+++ child.cpp
@@ -6,9 +6,15 @@
 using namespace std;
 using namespace std::chrono;
 
-int main(){
+int main()
+{
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    int Core = sysinfo.dwNumberOfProcessors;
 
-
+    HANDLE hData = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, TEXT("SharedMemory-data"));
+    HANDLE hResult = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, TEXT("SharedMemory-result"));
+    HANDLE hProcessInfo = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, TEXT("SharedMemory-ProcessInfo"));
 
     return 0;
 }
